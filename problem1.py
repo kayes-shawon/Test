42f54ad07bca6b1c1d3f100a3c4cf32fa213123c
kayes-shawon <shawon.kayes@gmail.com>
2020-01-10 20:35:00 +0600

    Completed problem 1

---
+++ problem1.py
@@ -0,0 +1,15 @@
+
+def print_depth(data, level=1):
+    """
+    Function to print all keys with their depth
+    """
+    # Iterarating through all dictionary items
+    for key, value in data.items():
+        if isinstance(value, dict):
+            print("{0} : {1}".format(key, level))
+            level +=1
+            print_depth(value, level)
+        else:
+            print("{0} : {1}".format(key, level))
+        
+print_depth({'key1':1, 'key2': { 'key3': 1, 'key4': {'key5':4}}})
